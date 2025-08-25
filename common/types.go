1d41a32c8c17977ee90dcc94acf4b54767cc49a0
wanwiset25 <wanwiset25@gmail.com>
2025-08-25 16:06:07 +0400

    update validator initial configs

--- common/types.go
+++ common/types.go
@@ -69,7 +69,6 @@ var (
 	XDCNativeAddressBinary                 = HexToAddress("0x0000000000000000000000000000000000000001")
 	LendingLockAddressBinary               = HexToAddress("0x0000000000000000000000000000000000000011")
 	MintedRecordAddressBinary              = HexToAddress("0x000000000000000000000000000000000000009a")
-	SwapAddressBinary                     = HexToAddress("0x000000000000000000000000000000000000008e") //confirm Address
 )
 
 var (
