408a4a07be0c25c29f48987caf8cc4ade7dfa6e5
wanwiset25 <wanwiset25@gmail.com>
2025-11-26 14:30:38 +0400

    copy engine_v2 files as initial engine_v2_subnet files

---
+++ consensus/XDPoS/engines/engine_v2_subnet/utils.go
@@ -0,0 +1,363 @@
+package engine_v2_subnet
+
+import (
+	"errors"
+	"fmt"
+	"math/big"
+
+	"github.com/XinFinOrg/XDPoSChain/accounts"
+	"github.com/XinFinOrg/XDPoSChain/common"
+	"github.com/XinFinOrg/XDPoSChain/consensus"
+	"github.com/XinFinOrg/XDPoSChain/consensus/XDPoS/utils"
+	"github.com/XinFinOrg/XDPoSChain/core/types"
+	"github.com/XinFinOrg/XDPoSChain/crypto"
+	"github.com/XinFinOrg/XDPoSChain/log"
+	"github.com/XinFinOrg/XDPoSChain/rlp"
+	"golang.org/x/crypto/sha3"
+)
+
+func sigHash(header *types.Header) (hash common.Hash) {
+	hasher := sha3.NewLegacyKeccak256()
+
+	enc := []interface{}{
+		header.ParentHash,
+		header.UncleHash,
+		header.Coinbase,
+		header.Root,
+		header.TxHash,
+		header.ReceiptHash,
+		header.Bloom,
+		header.Difficulty,
+		header.Number,
+		header.GasLimit,
+		header.GasUsed,
+		header.Time,
+		header.Extra,
+		header.MixDigest,
+		header.Nonce,
+		header.Validators,
+		header.Penalties,
+	}
+	if header.BaseFee != nil {
+		enc = append(enc, header.BaseFee)
+	}
+	if err := rlp.Encode(hasher, enc); err != nil {
+		panic("rlp.Encode fail: " + err.Error())
+	}
+	hasher.Sum(hash[:0])
+	return hash
+}
+
+func ecrecover(header *types.Header, sigcache *utils.SigLRU) (common.Address, error) {
+	// If the signature's already cached, return that
+	hash := header.Hash()
+	if address, known := sigcache.Get(hash); known {
+		return address, nil
+	}
+
+	// Recover the public key and the Ethereum address
+	pubkey, err := crypto.Ecrecover(sigHash(header).Bytes(), header.Validator)
+	if err != nil {
+		return common.Address{}, err
+	}
+	var signer common.Address
+	copy(signer[:], crypto.Keccak256(pubkey[1:])[12:])
+
+	sigcache.Add(hash, signer)
+	return signer, nil
+}
+
+// Get masternodes address from checkpoint Header. Only used for v1 last block
+func decodeMasternodesFromHeaderExtra(checkpointHeader *types.Header) []common.Address {
+	masternodes := make([]common.Address, (len(checkpointHeader.Extra)-utils.ExtraVanity-utils.ExtraSeal)/common.AddressLength)
+	for i := 0; i < len(masternodes); i++ {
+		copy(masternodes[i][:], checkpointHeader.Extra[utils.ExtraVanity+i*common.AddressLength:])
+	}
+	return masternodes
+}
+
+func UniqueSignatures(signatureSlice []types.Signature) ([]types.Signature, []types.Signature) {
+	keys := make(map[string]bool)
+	list := []types.Signature{}
+	duplicates := []types.Signature{}
+	for _, signature := range signatureSlice {
+		hexOfSig := common.Bytes2Hex(signature)
+		if _, value := keys[hexOfSig]; !value {
+			keys[hexOfSig] = true
+			list = append(list, signature)
+		} else {
+			duplicates = append(duplicates, signature)
+		}
+	}
+	return list, duplicates
+}
+
+func (x *XDPoS_v2) signSignature(signingHash common.Hash) (types.Signature, error) {
+	// Don't hold the signFn for the whole signing operation
+	x.signLock.RLock()
+	signer, signFn := x.signer, x.signFn
+	x.signLock.RUnlock()
+
+	if signFn == nil {
+		return nil, errors.New("signFn is nil")
+	}
+	signedHash, err := signFn(accounts.Account{Address: signer}, signingHash.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("error %v while signing hash", err)
+	}
+	return signedHash, nil
+}
+
+func (x *XDPoS_v2) verifyMsgSignature(signedHashToBeVerified common.Hash, signature types.Signature, masternodes []common.Address) (bool, common.Address, error) {
+	var signerAddress common.Address
+	if len(masternodes) == 0 {
+		return false, signerAddress, errors.New("empty masternode list detected when verifying message signatures")
+	}
+	// Recover the public key and the Ethereum address
+	pubkey, err := crypto.Ecrecover(signedHashToBeVerified.Bytes(), signature)
+	if err != nil {
+		return false, signerAddress, fmt.Errorf("error while verifying message: %v", err)
+	}
+
+	copy(signerAddress[:], crypto.Keccak256(pubkey[1:])[12:])
+	for _, mn := range masternodes {
+		if mn == signerAddress {
+			return true, signerAddress, nil
+		}
+	}
+
+	log.Warn("[verifyMsgSignature] signer is not part of masternode list", "signer", signerAddress, "masternodes", masternodes)
+	return false, signerAddress, nil
+}
+
+func (x *XDPoS_v2) getExtraFields(header *types.Header) (*types.QuorumCert, types.Round, []common.Address, error) {
+	var masternodes []common.Address
+
+	// last v1 block
+	if header.Number.Cmp(x.config.V2.SwitchBlock) == 0 {
+		masternodes = decodeMasternodesFromHeaderExtra(header)
+		return nil, types.Round(0), masternodes, nil
+	}
+
+	// v2 block
+	masternodes = x.GetMasternodesFromEpochSwitchHeader(header)
+	var decodedExtraField types.ExtraFields_v2
+	err := utils.DecodeBytesExtraFields(header.Extra, &decodedExtraField)
+	if err != nil {
+		log.Error("[getExtraFields] error on decode extra fields", "err", err, "extra", header.Extra)
+		return nil, types.Round(0), masternodes, err
+	}
+	return decodedExtraField.QuorumCert, decodedExtraField.Round, masternodes, nil
+}
+
+func (x *XDPoS_v2) GetRoundNumber(header *types.Header) (types.Round, error) {
+	// If not v2 yet, return 0
+	if header.Number.Cmp(x.config.V2.SwitchBlock) <= 0 {
+		return types.Round(0), nil
+	} else {
+		var decodedExtraField types.ExtraFields_v2
+		err := utils.DecodeBytesExtraFields(header.Extra, &decodedExtraField)
+		if err != nil {
+			return types.Round(0), err
+		}
+		return decodedExtraField.Round, nil
+	}
+}
+
+func (x *XDPoS_v2) GetSignersFromSnapshot(chain consensus.ChainReader, header *types.Header) ([]common.Address, error) {
+	snap, err := x.getSnapshot(chain, header.Number.Uint64(), false)
+	if err != nil {
+		return nil, err
+	}
+	return snap.NextEpochCandidates, err
+}
+
+func (x *XDPoS_v2) CalculateMissingRounds(chain consensus.ChainReader, header *types.Header) (*utils.PublicApiMissedRoundsMetadata, error) {
+	var missedRounds []utils.MissedRoundInfo
+	switchInfo, err := x.getEpochSwitchInfo(chain, header, header.Hash())
+	if err != nil {
+		return nil, err
+	}
+	masternodes := switchInfo.Masternodes
+	if len(masternodes) == 0 {
+		return nil, fmt.Errorf("masternodes is empty in CalculateMissingRounds, number = %v, hash %#x", header.Number, header.Hash())
+	}
+
+	// Loop through from the epoch switch block to the current "header" block
+	nextHeader := header
+	for nextHeader.Number.Cmp(switchInfo.EpochSwitchBlockInfo.Number) > 0 {
+		parentHeader := chain.GetHeaderByHash(nextHeader.ParentHash)
+		if parentHeader == nil {
+			return nil, fmt.Errorf("fail to get header by hash %v: ", nextHeader.ParentHash)
+		}
+		parentRound, err := x.GetRoundNumber(parentHeader)
+		if err != nil {
+			return nil, err
+		}
+		currRound, err := x.GetRoundNumber(nextHeader)
+		if err != nil {
+			return nil, err
+		}
+		// This indicates that an increment in the round number is missing during the block production process.
+		if parentRound+1 != currRound {
+			// We need to iterate from the parentRound to the currRound to determine which miner did not perform mining.
+			for i := parentRound + 1; i < currRound; i++ {
+				leaderIndex := uint64(i) % x.config.Epoch % uint64(len(masternodes))
+				whosTurn := masternodes[leaderIndex]
+				missedRounds = append(
+					missedRounds,
+					utils.MissedRoundInfo{
+						Round:            i,
+						Miner:            whosTurn,
+						CurrentBlockHash: nextHeader.Hash(),
+						CurrentBlockNum:  nextHeader.Number,
+						ParentBlockHash:  parentHeader.Hash(),
+						ParentBlockNum:   parentHeader.Number,
+					},
+				)
+			}
+		}
+		// Assign the pointer to the next one
+		nextHeader = parentHeader
+	}
+	missedRoundsMetadata := &utils.PublicApiMissedRoundsMetadata{
+		EpochRound:       switchInfo.EpochSwitchBlockInfo.Round,
+		EpochBlockNumber: switchInfo.EpochSwitchBlockInfo.Number,
+		MissedRounds:     missedRounds,
+	}
+
+	return missedRoundsMetadata, nil
+}
+
+func (x *XDPoS_v2) getBlockByEpochNumberInCache(chain consensus.ChainReader, estRound types.Round) *types.BlockInfo {
+	epochSwitchInCache := make([]*types.BlockInfo, 0)
+	for r := estRound; r < estRound+types.Round(x.config.Epoch); r++ {
+		blockInfo, ok := x.round2epochBlockInfo.Get(r)
+		if ok && blockInfo != nil {
+			epochSwitchInCache = append(epochSwitchInCache, blockInfo)
+		}
+	}
+	if len(epochSwitchInCache) == 1 {
+		return epochSwitchInCache[0]
+	} else if len(epochSwitchInCache) == 0 {
+		return nil
+	}
+	// when multiple cache hits, need to find the one in main chain
+	for _, blockInfo := range epochSwitchInCache {
+		header := chain.GetHeaderByNumber(blockInfo.Number.Uint64())
+		if header == nil {
+			continue
+		}
+		if header.Hash() == blockInfo.Hash {
+			return blockInfo
+		}
+	}
+	return nil
+}
+
+func (x *XDPoS_v2) binarySearchBlockByEpochNumber(chain consensus.ChainReader, targetEpochNum uint64, start, end uint64) (*types.BlockInfo, *types.Header, error) {
+	// `end` must be larger than the target and `start` could be the target
+	for start < end {
+		header := chain.GetHeaderByNumber((start + end) / 2)
+		if header == nil {
+			return nil, nil, errors.New("header nil in binary search")
+		}
+		isEpochSwitch, epochNum, err := x.IsEpochSwitch(header)
+		if err != nil {
+			return nil, nil, err
+		}
+		if epochNum == targetEpochNum {
+			_, round, _, err := x.getExtraFields(header)
+			if err != nil {
+				return nil, nil, err
+			}
+			if isEpochSwitch {
+				return &types.BlockInfo{
+					Hash:   header.Hash(),
+					Round:  round,
+					Number: header.Number,
+				}, header, nil
+			} else {
+				end = header.Number.Uint64()
+				// trick to shorten the search
+				estStart := uint64(0)
+				// if statement to avoid negative underflow
+				roundModEpoch := uint64(round) % x.config.Epoch
+				if end >= roundModEpoch {
+					estStart = end - roundModEpoch
+				}
+
+				if start < estStart {
+					start = estStart
+				}
+			}
+		} else if epochNum > targetEpochNum {
+			end = header.Number.Uint64()
+		} else if epochNum < targetEpochNum {
+			// if start keeps the same, means no result and the search is over
+			nextStart := header.Number.Uint64()
+			if nextStart == start {
+				break
+			}
+			start = nextStart
+		}
+	}
+	return nil, nil, errors.New("no epoch switch header in binary search (all rounds in this epoch are missed, which is very rare)")
+}
+
+func (x *XDPoS_v2) GetBlockByEpochNumber(chain consensus.ChainReader, targetEpochNum uint64) (*types.BlockInfo, error) {
+	currentHeader := chain.CurrentHeader()
+	if currentHeader == nil {
+		return nil, errors.New("current header is nil")
+	}
+	epochSwitchInfo, err := x.getEpochSwitchInfo(chain, currentHeader, currentHeader.Hash())
+	if err != nil {
+		return nil, err
+	}
+	epochNum := x.config.V2.SwitchEpoch + uint64(epochSwitchInfo.EpochSwitchBlockInfo.Round)/x.config.Epoch
+	// if current epoch is this epoch, we early return the result
+	if targetEpochNum == epochNum {
+		return epochSwitchInfo.EpochSwitchBlockInfo, nil
+	}
+	if targetEpochNum > epochNum {
+		return nil, errors.New("input epoch number > current epoch number")
+	}
+	if targetEpochNum < x.config.V2.SwitchEpoch {
+		return nil, errors.New("input epoch number < v2 begin epoch number")
+	}
+	// the block's round should be in [estRound,estRound+Epoch-1]
+	estRound := types.Round((targetEpochNum - x.config.V2.SwitchEpoch) * x.config.Epoch)
+	// check the round2epochBlockInfo cache
+	blockInfo := x.getBlockByEpochNumberInCache(chain, estRound)
+	if blockInfo != nil {
+		return blockInfo, nil
+	}
+	// if cache miss, we do search
+	epoch := big.NewInt(int64(x.config.Epoch))
+	estblockNumDiff := new(big.Int).Mul(epoch, big.NewInt(int64(epochNum-targetEpochNum)))
+	estBlockNum := new(big.Int).Sub(epochSwitchInfo.EpochSwitchBlockInfo.Number, estblockNumDiff)
+	if estBlockNum.Cmp(x.config.V2.SwitchBlock) == -1 {
+		estBlockNum.Set(x.config.V2.SwitchBlock)
+	}
+	// if the targrt is close, we search brute-forcily
+	closeEpochNum := uint64(2)
+	if closeEpochNum >= epochNum-targetEpochNum {
+		estBlockHeader := chain.GetHeaderByNumber(estBlockNum.Uint64())
+		if estBlockHeader == nil {
+			return nil, fmt.Errorf("fail to get est block header by number: %v", estBlockNum)
+		}
+		epochSwitchInfos, err := x.GetEpochSwitchInfoBetween(chain, estBlockHeader, currentHeader)
+		if err != nil {
+			return nil, err
+		}
+		for _, info := range epochSwitchInfos {
+			epochNum := x.config.V2.SwitchEpoch + uint64(info.EpochSwitchBlockInfo.Round)/x.config.Epoch
+			if epochNum == targetEpochNum {
+				return info.EpochSwitchBlockInfo, nil
+			}
+		}
+	}
+	// else, we use binary search
+	blockInfo, _, err = x.binarySearchBlockByEpochNumber(chain, targetEpochNum, estBlockNum.Uint64(), epochSwitchInfo.EpochSwitchBlockInfo.Number.Uint64())
+	return blockInfo, err
+}
---
+++ consensus/XDPoS/engines/engine_v2_subnet/verifyHeader.go
@@ -0,0 +1,203 @@
+package engine_v2_subnet
+
+import (
+	"bytes"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/XinFinOrg/XDPoSChain/common"
+	"github.com/XinFinOrg/XDPoSChain/common/hexutil"
+	"github.com/XinFinOrg/XDPoSChain/consensus"
+	"github.com/XinFinOrg/XDPoSChain/consensus/XDPoS/utils"
+	"github.com/XinFinOrg/XDPoSChain/consensus/misc"
+	"github.com/XinFinOrg/XDPoSChain/consensus/misc/eip1559"
+	"github.com/XinFinOrg/XDPoSChain/core/types"
+	"github.com/XinFinOrg/XDPoSChain/log"
+)
+
+// Verify individual header
+func (x *XDPoS_v2) verifyHeader(chain consensus.ChainReader, header *types.Header, parents []*types.Header, fullVerify bool) error {
+	if !x.isInitilised {
+		if err := x.initial(chain, header); err != nil {
+			return err
+		}
+	}
+
+	_, ok := x.verifiedHeaders.Get(header.Hash())
+	if ok {
+		return nil
+	}
+
+	if header.Number == nil {
+		return utils.ErrUnknownBlock
+	}
+
+	if len(header.Validator) == 0 {
+		// This should never happen, if it does, then it means the peer is sending us invalid data.
+		return consensus.ErrNoValidatorSignatureV2
+	} else if len(header.Validator) != 65 {
+		return fmt.Errorf("invalid validator signature length %d, want 65", len(header.Validator))
+	}
+
+	if fullVerify {
+		// Don't waste time checking blocks from the future
+		if header.Time > uint64(time.Now().Unix()) {
+			return consensus.ErrFutureBlock
+		}
+	}
+
+	// Ensure that the block's timestamp isn't too close to it's parent
+	var parent *types.Header
+	number := header.Number.Uint64()
+
+	if len(parents) > 0 {
+		parent = parents[len(parents)-1]
+	} else {
+		parent = chain.GetHeader(header.ParentHash, number-1)
+	}
+	if parent == nil || parent.Number.Uint64() != number-1 || parent.Hash() != header.ParentHash {
+		return consensus.ErrUnknownAncestor
+	}
+
+	// Ensure gas limit is consistent with parent
+	err := misc.VerifyGaslimit(parent.GasLimit, header.GasLimit)
+	if err != nil && parent.Number.Uint64() != 0 { // skip genesis block
+		return err
+	}
+	// Ensure gas used is less than or equal to gas limit
+	if header.GasUsed > header.GasLimit {
+		return fmt.Errorf("gas used exceeded gaslimit, gas used: %d, gas limit: %d", header.GasUsed, header.GasLimit)
+	}
+
+	// Verify this is a true v2 block first
+	quorumCert, round, _, err := x.getExtraFields(header)
+	if err != nil {
+		log.Warn("[verifyHeader] decode extra field error", "err", err)
+		return utils.ErrInvalidV2Extra
+	}
+	if quorumCert == nil {
+		log.Warn("[verifyHeader] quorumCert is nil")
+		return utils.ErrInvalidQuorumCert
+	}
+
+	minePeriod := uint64(x.config.V2.Config(uint64(round)).MinePeriod)
+	if parent.Number.Uint64() >= x.config.V2.SwitchBlock.Uint64() && parent.Time+minePeriod > header.Time {
+		log.Warn("[verifyHeader] Fail to verify header due to invalid timestamp", "ParentTime", parent.Time, "MinePeriod", minePeriod, "HeaderTime", header.Time, "Hash", header.Hash().Hex())
+		return utils.ErrInvalidTimestamp
+	}
+
+	if round <= quorumCert.ProposedBlockInfo.Round {
+		return utils.ErrRoundInvalid
+	}
+
+	err = x.verifyQC(chain, quorumCert, parent)
+	if err != nil {
+		log.Warn("[verifyHeader] fail to verify QC", "QCNumber", quorumCert.ProposedBlockInfo.Number, "QCsigLength", len(quorumCert.Signatures))
+		return err
+	}
+	// Nonces must be 0x00..0 or 0xff..f, zeroes enforced on checkpoints
+	if !bytes.Equal(header.Nonce[:], utils.NonceAuthVote) && !bytes.Equal(header.Nonce[:], utils.NonceDropVote) {
+		return utils.ErrInvalidVote
+	}
+	// Ensure that the mix digest is zero as we don't have fork protection currently
+	if header.MixDigest != (common.Hash{}) {
+		return utils.ErrInvalidMixDigest
+	}
+	// Ensure that the block doesn't contain any uncles which are meaningless in XDPoS_v2.
+	if header.UncleHash != utils.UncleHash {
+		return utils.ErrInvalidUncleHash
+	}
+	// Verify the header's EIP-1559 attributes.
+	if err := eip1559.VerifyEip1559Header(chain.Config(), header); err != nil {
+		return err
+	}
+	if header.Difficulty.Cmp(big.NewInt(1)) != 0 {
+		return utils.ErrInvalidDifficulty
+	}
+
+	var masterNodes []common.Address
+	isEpochSwitch, _, err := x.IsEpochSwitch(header) // Verify v2 block that is on the epoch switch
+	if err != nil {
+		log.Error("[verifyHeader] error when checking if header is epoch switch header", "Hash", header.Hash(), "Number", header.Number, "Error", err)
+		return err
+	}
+	if isEpochSwitch {
+		if !bytes.Equal(header.Nonce[:], utils.NonceDropVote) {
+			return utils.ErrInvalidCheckpointVote
+		}
+		if len(header.Validators) == 0 {
+			return utils.ErrEmptyEpochSwitchValidators
+		}
+		if len(header.Validators)%common.AddressLength != 0 {
+			return utils.ErrInvalidCheckpointSigners
+		}
+
+		localMasterNodes, localPenalties, err := x.calcMasternodes(chain, header.Number, header.ParentHash, round)
+		masterNodes = localMasterNodes
+		if err != nil {
+			log.Error("[verifyHeader] Fail to calculate master nodes list with penalty", "Number", header.Number, "Hash", header.Hash())
+			return err
+		}
+
+		validatorsAddress := common.ExtractAddressFromBytes(header.Validators)
+		if !utils.CompareSignersLists(localMasterNodes, validatorsAddress) {
+			for i, addr := range localMasterNodes {
+				log.Warn("[verifyHeader] localMasterNodes", "i", i, "addr", addr.Hex())
+			}
+			for i, addr := range validatorsAddress {
+				log.Warn("[verifyHeader] validatorsAddress", "i", i, "addr", addr.Hex())
+			}
+			return utils.ErrValidatorsNotLegit
+		}
+
+		penaltiesAddress := common.ExtractAddressFromBytes(header.Penalties)
+		if !utils.CompareSignersLists(localPenalties, penaltiesAddress) {
+			for i, addr := range localPenalties {
+				log.Warn("[verifyHeader] localPenalties", "i", i, "addr", addr.Hex())
+			}
+			for i, addr := range penaltiesAddress {
+				log.Warn("[verifyHeader] penaltiesAddress", "i", i, "addr", addr.Hex())
+			}
+			return utils.ErrPenaltiesNotLegit
+		}
+
+	} else {
+		if len(header.Validators) != 0 {
+			log.Warn("[verifyHeader] Validators shall not have values in non-epochSwitch block", "Hash", header.Hash(), "Number", header.Number, "header.Validators", header.Validators)
+			return utils.ErrInvalidFieldInNonEpochSwitch
+		}
+		if len(header.Penalties) != 0 {
+			log.Warn("[verifyHeader] Penalties shall not have values in non-epochSwitch block", "Hash", header.Hash(), "Number", header.Number, "header.Penalties", header.Penalties)
+			return utils.ErrInvalidFieldInNonEpochSwitch
+		}
+		masterNodes = x.GetMasternodes(chain, header)
+	}
+
+	verified, validatorAddress, err := x.verifyMsgSignature(sigHash(header), header.Validator, masterNodes)
+	if err != nil {
+		for index, mn := range masterNodes {
+			log.Error("[verifyHeader] masternode list during validator verification", "Masternode Address", mn.Hex(), "index", index)
+		}
+		log.Error("[verifyHeader] Error while verifying header validator signature", "BlockNumber", header.Number, "Hash", header.Hash().Hex(), "validator in hex", hexutil.Encode(header.Validator))
+		return err
+	}
+	if !verified {
+		log.Warn("[verifyHeader] Fail to verify the block validator as the validator address not within the masternode list", "BlockNumber", header.Number, "Hash", header.Hash().Hex(), "validatorAddress", validatorAddress.Hex())
+		return utils.ErrValidatorNotWithinMasternodes
+	}
+	if validatorAddress != header.Coinbase {
+		log.Warn("[verifyHeader] Header validator and coinbase address not match", "BlockNumber", header.Number, "Hash", header.Hash().Hex(), "validatorAddress", validatorAddress.Hex(), "coinbase", header.Coinbase.Hex())
+		return utils.ErrCoinbaseAndValidatorMismatch
+	}
+	// Check the proposer is the leader
+	curIndex := utils.Position(masterNodes, validatorAddress)
+	leaderIndex := uint64(round) % x.config.Epoch % uint64(len(masterNodes))
+	if masterNodes[leaderIndex] != validatorAddress {
+		log.Warn("[verifyHeader] Invalid blocker proposer, not its turn", "curIndex", curIndex, "leaderIndex", leaderIndex, "Hash", header.Hash().Hex(), "masterNodes[leaderIndex]", masterNodes[leaderIndex], "validatorAddress", validatorAddress)
+		return utils.ErrNotItsTurn
+	}
+
+	x.verifiedHeaders.Add(header.Hash(), struct{}{})
+	return nil
+}
