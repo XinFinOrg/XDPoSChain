ee5799546677401d26edaba033e2f881a5067961
benjamin2024 <benjamin.franklin.git@gmail.com>
2026-05-19 18:04:27 +0800

    update version v2.7.0

--- params/version.go
+++ params/version.go
@@ -22,8 +22,8 @@ import (
 
 const (
 	VersionMajor = 2        // Major version component of the current release
-	VersionMinor = 6        // Minor version component of the current release
-	VersionPatch = 8        // Patch version component of the current release
+	VersionMinor = 7        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
