a5dd1313de55e9f24b50f411eb823f4768984bd4
benjamin2024 <benjamin.franklin.git@gmail.com>
2026-09-25 22:22:52 +0800

    update to correct round number and config

--- core/chainconfig_equal_test.go
+++ core/chainconfig_equal_test.go
@@ -478,9 +478,9 @@ func TestHashChainConfigSemanticGoldenVectors(t *testing.T) {
 		want string
 	}{
 		{name: "nil", cfg: nil, want: "99be5efb88ca2013bd8e4eb035fd42d5245468fe9afa70d8ba9c1c419a48c4e8"},
-		{name: "testnet", cfg: params.TestnetChainConfig.Clone(), want: "26f0d8c8c5e1a70429ed3d000190b6f57cafece1356cc21beed6ee5b2a6802dd"},
+		{name: "testnet", cfg: params.TestnetChainConfig.Clone(), want: "d3bd0cda5358a87b5c08774590fb7e0bf64e20007796d622f2b4e89f9400a139"},
 		{name: "mainnet", cfg: params.XDCMainnetChainConfig.Clone(), want: "9b0ecfced732164016e61836ed57a626be9bffb3df283036e3337811a5feed3b"},
-		{name: "testnet-berlin-drift", cfg: testnetBerlinDrift, want: "da188df184e73727c421908c2f8d833c89f977e2c0d6f363bdf096a2530a7423"},
+		{name: "testnet-berlin-drift", cfg: testnetBerlinDrift, want: "43979250b2a4a93b70db816ac696d4c88dce46b946ca5a05df3bd0ef499f76db"},
 	}
 
 	for _, test := range tests {
